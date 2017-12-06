0423f202fcc8af86b102a7e527e76eb5783ecd86
Lukas Dratwa <lukas.dratwa@stud.tu-darmstadt.de>
2017-12-06 18:57:01 +0100

    Worked on displaying of dataset data

--- public/javascripts/controllers/viewDataSetController.js
+++ public/javascripts/controllers/viewDataSetController.js
@@ -6,14 +6,25 @@ dataSetController.controller("viewDataSetController", function ($scope, $http, $
         $scope.id = id;
 
         var requestUrl = "/dataset?id=" + id;
-        console.log(requestUrl);
         var request = $http.get(requestUrl);
         request.success(function(data) {
+            enrichDataset(data);
             $scope.dataset = data;
+
+
             console.log(data);
         });
         request.error(function(err){
             console.log('Error: ' + err);
         });
+    }
+
+
+    function enrichDataset(dataset) {
+        for(var i in dataset.vehicles) {
+            var vehicle = dataset.vehicles[i];
+
+            vehicle.nr = parseInt(i) +1;
+        }
     }
 });
