a62a326b122ecf7fe37b43a17c044105e532659d
Lukas Dratwa <lukas.dratwa@stud.tu-darmstadt.de>
2018-01-07 20:18:12 +0100

    Nearly finished neighbourhood generation function

--- public/javascripts/controllers/integratedTabuSearchController.js
+++ public/javascripts/controllers/integratedTabuSearchController.js
@@ -1,5 +1,17 @@
 var tabuController = angular.module('integratedTabuSearchApp', []);
 
+function getRandomNumber(min, max) {
+    return Math.floor((Math.random() * (max + 1)) + min);
+}
+
+function getMinOfPair(first, second) {
+    if(first <= second) {
+        return first;
+    } else {
+        return second;
+    }
+}
+
 function ParameterSet(parameters) {
     this.parameters = parameters;
 
@@ -147,11 +159,11 @@ function Solution(vehicles, parameters, ratios) {
                         if(ratio.prio == 0) {
                             // High priority ratio
                             this.actHighPrioViolations++;
-                            this.actHighPrioViolationsExtended.push([x, endIndex])
+                            this.actHighPrioViolationsExtended.push([parseInt(x), endIndex])
                         } else {
                             // Low priority ratio
                             this.actLowPrioViolations++;
-                            this.actLowPrioViolationsExtended.push([x, endIndex])
+                            this.actLowPrioViolationsExtended.push([parseInt(x), endIndex])
                         }
                     }
                 }
@@ -204,12 +216,16 @@ function Solution(vehicles, parameters, ratios) {
             }
         }
         // EOF 1.
+
+
+        console.log("Performed obl. update after every move", this);
     };
     this.obligatoryUpdateAfterEveryMove();
     this.updateActViolations();
 
     this.insertVehicle = function(index, vehicle) {
         this.vehicles.splice(index, 0, vehicle);
+        this.obligatoryUpdateAfterEveryMove();
         return this;
     };
 
@@ -217,6 +233,7 @@ function Solution(vehicles, parameters, ratios) {
         var tmpVehicle = this.vehicles[firstIndex];
         this.vehicles[firstIndex] = this.vehicles[secondIndex];
         this.vehicles[secondIndex] = this.vehicles[firstIndex];
+        this.obligatoryUpdateAfterEveryMove();
         return this;
     };
 
@@ -284,7 +301,6 @@ function costFunctionG(s) {
     /** IMPORTANT NOTES
      * "At each iteration, the value of z is multiplied by 2 if the current solution is infeasible, and divided
      * by 2 otherwise." --> To get a mix of feasible and infeasible solutions.
-     *
      */
 }
 
@@ -292,12 +308,45 @@ function costFunctionG(s) {
  * Only a subset of all potential exchanges is considered through sampling.
  *
  * @param s - a given solution
+ * @param iterationCounter - the actual iteration counter
  */
-function getNeighbourhood(s) {
-    // 1. Assign each pair of positions (i, j) an integer µij in [0, n-1] ([0, Eta-1]) and in each iteration t, consider
-    //    this pair only if µij = t (mod n) --> (mod Eta)
-    //    --> Re-assign µij every Eta iterations
+function getNeighbourhood(s, iterationCounter) {
+    var neighbourhood = [];
 
+    // 1. Assign each pair of positions (i, j) an integer µij in [0, n-1] ([0, Eta-1]) and in each iteration t, consider
+    //    this pair only if µij = t (mod n) <--> (mod Eta)
+    //    --> Eta = min {n, 200}
+    //    --> Re-assign µij every Eta iterations (when the pair was considered)
+    var etaMax = s.parameterSet.getParamWithIdent("n");
+    var eta = getMinOfPair(s.vehicles.length, etaMax.value);
+
+    // Init of the lock array
+    for(var i in s.vehicles) {
+        var vehicleI = s.vehicles[i];
+
+        if(typeof vehicleI.lockArray === "undefined") {
+            vehicleI.lockArray = [];
+
+            for(var j in s.vehicles) {
+                vehicleI.lockArray.push(getRandomNumber(0, eta - 1));
+            }
+        }
+    }
+
+    for(var i in s.vehicles) {
+        var vehicleI = s.vehicles[i];
+
+        // Consider only if condition applies
+        for(var j in s.vehicles) {
+            var verhicleJ = s.vehicles[j];
+
+            if(i != j && vehicleI.lockArray[j] == iterationCounter % eta) {
+                // Re-assign random number
+                vehicleI.lockArray[i] = getRandomNumber(0, eta - 1);
+                console.log("Would look at: " + i + " " + j);
+            }
+        }
+    }
 
     // Only swap a pair if you improve the current solution
 
@@ -309,6 +358,9 @@ function getNeighbourhood(s) {
     * -----> Swaps are only allowed if this criterion is met for both cars
     */
 
+
+    neighbourhood.push(s); // FIXME delete me
+    return neighbourhood;
 }
 
 // To escape a local optimum and reach new good solutions
@@ -334,9 +386,7 @@ function continuousDiversification() {
 }
 
 
-function PertubationMechanisms(solution) {
-    this.solution = solution;
-
+function PertubationMechanisms() {
     this.randomSwaps = function() {
 
     };
@@ -361,6 +411,10 @@ function PertubationMechanisms(solution) {
 
     };
 
+    this.performPertubation = function(solution) {
+        return solution;
+    };
+
     /* Pertubation is applied in 3a
      *
      * After every pertubation the parameters a, b and y are modified. Three different configurations:
@@ -371,35 +425,60 @@ function PertubationMechanisms(solution) {
      */
 }
 
-function performTabuSearch() {
-    // Perform till no improvement of f(s) since k (Kappa) iterations
+function solutionSatisfiesAcceptanceCriterion() {
+    /*
+     * - If f1 or f2 was used in the pertuabation and if a better solution in 3b was found, it replaces
+     *   the current solution
+     * - If f3 ... TODO ?!
+     */
+
+    return false;
+}
+
+function performTabuSearch(solution, iterationCounter) {
+    var neighbourhood = getNeighbourhood(solution, iterationCounter);
+
+    return neighbourhood[0];
+}
+
+
+function performIteratedTabuSearch(s) {
+    var sImproved, sImprovedCosts, sCurrent, sLocalOptimum;
+    var paramSet = s.parameterSet;
+    var iterationCounter = 1;
+    var pertubationMechanism = new PertubationMechanisms();
+
+    // 1. Init
+    var s0 = s;
+
+    // 2. Apply tabu search on s0 to obtain improved solution sImproved
+    sImproved = performTabuSearch(s0, iterationCounter);
+
+    // 3. Perform till no improvement of f(s) since k (Kappa) iterations
+    var iterationsWithoutImprovement = 0;
+    while(iterationsWithoutImprovement < paramSet.getParamWithIdent("k").value - 1) {
+        iterationCounter++;
+
+        // a) Apply pertubation on sImproved to obtain sCurrent
+        sCurrent = pertubationMechanism.performPertubation(sImproved);
+
+        // b) Apply tabu search on sCurrent to obtain sLocalOptimum
+        sLocalOptimum = performTabuSearch(sCurrent, iterationCounter);
+
+        // c) If sLocalOptimum satisfies the acceptance criterion set it to sImproved
+        if(solutionSatisfiesAcceptanceCriterion(sLocalOptimum)) {
+            sImproved = sLocalOptimum;
+            iterationsWithoutImprovement = 0;
+        } else {
+            iterationsWithoutImprovement++;
+        }
+    }
+
+    // 4. Return the best found solution
+    return sImproved;
 }
 
 tabuController.controller("integratedTabuSearchController", function ($scope, $http, $location) {
-    function performITS(data) {
-        // !! Sort the vehicles
-        data.dataset.vehicles.sort(function(a, b) {
-            if(a.orderNr > b.orderNr) {
-                return 1;
-            } else if(a.orderNr < b.orderNr) {
-                return -1;
-            } else {
-                return 0;
-            }
-        });
-
-        console.log("Start ITS: " , data);
-
-        var s1 = new Solution(data.dataset.vehicles, data.parameters, data.dataset.ratios);
-        var s2 = new Solution(data.dataset.vehicles, data.parameters, data.dataset.ratios);
-        console.log(s1);
-
-        $scope.standardParameters = data.parameters;
-        $scope.dataset = data.dataset;
-        $scope.optObjectives = data.optObjective;
-        $scope.optObjectivesNames = data.optObjective.orderDisplayingNames;
-    }
-
     var id = $location.search().id;
     $scope.performTabuSearch = false;
     $scope.selectedDatasetId = "";
@@ -411,7 +490,27 @@ tabuController.controller("integratedTabuSearchController", function ($scope, $h
         $scope.performTabuSearch = true;
         var tabuSearchRequest = $http.get('/gettabusearch?id=' + id);
         tabuSearchRequest.success(function(data) {
-            performITS(data);
+            // !! Sort the vehicles
+            data.dataset.vehicles.sort(function(a, b) {
+                if(a.orderNr > b.orderNr) {
+                    return 1;
+                } else if(a.orderNr < b.orderNr) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            });
+
+            console.log("Start ITS: " , data);
+
+            $scope.standardParameters = data.parameters;
+            $scope.dataset = data.dataset;
+            $scope.optObjectives = data.optObjective;
+            $scope.optObjectivesNames = data.optObjective.orderDisplayingNames;
+
+            var s = new Solution(data.dataset.vehicles, data.parameters, data.dataset.ratios);
+            console.log("Solution ", s);
+            performIteratedTabuSearch(s);
         });
         tabuSearchRequest.error(function(err){
             console.log('Error: ' + err);
